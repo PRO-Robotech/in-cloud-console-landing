afbefa3c60dbc0a99b4fef7caa89783c54129ec1
typescreep <typescreep@prorobotech.ru>
2025-08-31 22:33:52 +0300

    to docs link test

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,5 +3,12 @@ import { HomeTemplate } from '@templates'
 import { THomePage } from '@localTypes/pageTypes'
 
 export const HomePage: FC<Omit<THomePage, 'meta'>> = ({ title }) => {
-  return <HomeTemplate>{title}</HomeTemplate>
+  return (
+    <HomeTemplate>
+      <div>{title}</div>
+      <div>
+        <a href="/docs/">To docs test link</a>
+      </div>
+    </HomeTemplate>
+  )
 }
